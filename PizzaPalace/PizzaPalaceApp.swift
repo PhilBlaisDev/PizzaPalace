8094cf935806a4df80d9f5ab6ed23da24eb48936
Philippe <phil@aircodedesign.com>
2021-06-05 09:32:33 -0700

    Initial Commit

---
+++ PizzaPalace/PizzaPalaceApp.swift
@@ -0,0 +1,20 @@
+//
+//  PizzaPalaceApp.swift
+//  PizzaPalace
+//
+//  Created by Philippe Blais on 2021-06-05.
+//
+
+import SwiftUI
+
+@main
+struct PizzaPalaceApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
